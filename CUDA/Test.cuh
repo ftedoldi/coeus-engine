08d3bd49d0f5ef6f49d28512b9019321072928fc
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-06-09 18:00:27 +0200

    Added CUDA workspace directory with powershell script that compiles all the CUDA file that are present in the workspace

---
+++ CUDA/Test.cuh
@@ -0,0 +1,17 @@
+#ifndef TEST_H
+#define TEST_H
+
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int __declspec(dllexport) call();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // KERNEL_H
---
+++ CUDA/buildCuda.ps1
@@ -0,0 +1,14 @@
+cd CUDA;
+
+foreach ($line in ls) {
+    $line = $line.toString();
+
+    if ($line.Substring($line.length - 2) -eq "cu") {
+        $name, $extension = $line.Split(".");
+        nvcc -o Shared/$name.dll --shared $line;
+
+        mv Shared/$name.dll ../Build;
+    }
+}
+
+cd ..;
