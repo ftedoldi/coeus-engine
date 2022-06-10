340a8e43af6acd618fcd3ecb107288e67def0183
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-06-10 14:05:11 +0200

    Implementation of the static method for the cross product between two 3D vectors

--- Math/Vector/Vector3.hpp
+++ Math/Vector/Vector3.hpp
@@ -5,7 +5,18 @@ namespace Athena
 {
     class Vector3
     {
+        public:
 
+            Vector3();
+            Vector3(const Scalar& x, const Scalar& y, const Scalar& z);
+            Vector3(const Vector2& vector, const Scalar& z);
+            Vector3(const Vector3& vector);
+        
+            static Vector3 cross(const Vector3& vector1, const Vector3& vector2) {
+                return Vector3();
+            }
+
+            Vector3 cross(const Vector3& vector);
     };
 }
 
