e6f5b723eb8f98d129c6471209e4636a06388d27
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-06-13 18:30:20 +0200

    Added Vector3 implementation

--- Math/Vector/Vector3.hpp
+++ Math/Vector/Vector3.hpp
@@ -1,17 +1,27 @@
 #ifndef VECTOR3_HPP
 #define VECTOR3_HPP
 
-#include "./Vector2.hpp"
+#include <Vector.hpp>
+#include <Vector2.hpp>
 
 namespace Athena
 {
     typedef float Scalar;
 
+    template<typename K>
+    struct Vector3Coordinates
+    {
+        K x;
+        K y;
+        K z;
+    };
+
     class Vector2;
 
-    class Vector3
+    class Vector3 : Vector<Vector3Coordinates<Scalar>, Vector3, Scalar>
     {
         public:
+            Vector3Coordinates<Scalar> coordinates;
 
             Vector3();
             Vector3(const Scalar& x, const Scalar& y, const Scalar& z);
@@ -19,10 +29,45 @@ namespace Athena
             Vector3(const Vector3& vector);
         
             static Vector3 cross(const Vector3& vector1, const Vector3& vector2) {
-                return Vector3();
+                return Vector3(
+                    vector1.coordinates.y * vector2.coordinates.z - vector1.coordinates.z * vector2.coordinates.y,
+                    vector1.coordinates.z * vector2.coordinates.x - vector1.coordinates.z * vector2.coordinates.z,
+                    vector1.coordinates.x * vector2.coordinates.y - vector1.coordinates.y * vector2.coordinates.x
+                );
             }
 
-            Vector3 cross(const Vector3& vector);
+            Vector3 cross(const Vector3& vector) const;
+            Scalar dot(const Vector3& vector) const;
+
+            Scalar magnitude() const;
+            Scalar squareMagnitude() const;
+            Vector3 normalized() const;
+            void normalize();
+
+            Scalar operator [] (const short& i) const;
+            Scalar& operator [] (const short& i);
+
+            Scalar operator * (const Vector3& vector) const;
+            Vector3 operator + (const Vector3& vector) const;
+            Vector3 operator - (const Vector3& vector) const;
+            bool operator == (const Vector3& vector) const;
+            Vector3 operator - () const;
+            Vector3 operator * (const Scalar& k) const;
+            Vector3 operator / (const Scalar& k) const;
+
+            void operator += (const Vector3& vector);
+            void operator -= (const Vector3& vector);
+            void operator *= (const Scalar& k);
+            void operator /= (const Scalar& k);
+
+            Scalar angleBetween (const Vector3& vector) const;
+
+            Vector3 lerp(const Vector3& vector, const Scalar& t) const;
+            
+            bool isZero() const;
+
+            bool areEquals(const Vector3& vector) const;
+            void print() const;
     };
 }
 
---
+++ Math/Vector/src/Vector3.cpp
@@ -0,0 +1,160 @@
+#include <Vector3.hpp>
+
+namespace Athena {
+    Vector3::Vector3() {
+        coordinates = Vector3Coordinates<Scalar>();
+        this->coordinates.x = (Scalar) 0.0;
+        this->coordinates.y = (Scalar) 0.0;
+        this->coordinates.z = (Scalar) 0.0;
+    }
+
+    Vector3::Vector3(const Scalar& x, const Scalar& y, const Scalar& z) {
+        coordinates = Vector3Coordinates<Scalar>();
+        this->coordinates.x = x;
+        this->coordinates.y = y;
+        this->coordinates.z = z;
+    }
+
+    Vector3::Vector3(const Vector2& vector, const Scalar& z) {
+        coordinates = Vector3Coordinates<Scalar>();
+        this->coordinates.x = vector.coordinates.x;
+        this->coordinates.y = vector.coordinates.y;
+        this->coordinates.z = z;
+    }
+
+    Vector3::Vector3(const Vector3& vector) {
+        coordinates = Vector3Coordinates<Scalar>();
+        this->coordinates.x = vector.coordinates.x;
+        this->coordinates.y = vector.coordinates.y;
+        this->coordinates.z = vector.coordinates.z;
+    }
+
+    Scalar Vector3::dot(const Vector3& vector) const {
+        return this->coordinates.x * vector.coordinates.x +
+                this->coordinates.y * vector.coordinates.y +
+                this->coordinates.z * vector.coordinates.z;
+    }
+
+    Vector3 Vector3::cross(const Vector3& vector) const {
+        return Vector3::cross(*this, vector);
+    }
+
+    Scalar Vector3::magnitude() const {
+        return std::sqrt(this->squareMagnitude());
+    }
+
+    Scalar Vector3::squareMagnitude() const {
+        return (this->coordinates.x * this->coordinates.x) + (this->coordinates.y * this->coordinates.y) + (this->coordinates.z * this->coordinates.z);
+    }
+
+    Vector3 Vector3::normalized() const {
+        return *this / this->magnitude();
+    }
+
+    void Vector3::normalize() {
+        *this /= this->magnitude();
+    }
+
+    Scalar Vector3::operator [] (const short& i) const {
+        if (i == 0)
+            return this->coordinates.x;
+        if (i == 1)
+            return this->coordinates.y;
+        if (i == 2)
+            return this->coordinates.z;
+
+        throw std::invalid_argument("INDEX_OUT_OF_RANGE::in Vector3 the index might be either 0 or 1");
+    }
+
+    Scalar& Vector3::operator [] (const short& i) {
+        if (i == 0)
+            return this->coordinates.x;
+        if (i == 1)
+            return this->coordinates.y;
+        if (i == 2)
+            return this->coordinates.z;
+        
+        throw std::invalid_argument("INDEX_OUT_OF_RANGE::in Vector3 the index might be either 0 or 1");
+    }
+
+    Scalar Vector3::operator * (const Vector3& vector) const {
+        return this->dot(vector);
+    }
+
+    Vector3 Vector3::operator + (const Vector3& vector) const {
+        return Vector3(this->coordinates.x + vector.coordinates.x, 
+            this->coordinates.y + vector.coordinates.y,
+            this->coordinates.z + vector.coordinates.z);
+    }
+
+    Vector3 Vector3::operator - (const Vector3& vector) const {
+            return Vector3(this->coordinates.x - vector.coordinates.x, 
+                this->coordinates.y - vector.coordinates.y,
+                this->coordinates.z - vector.coordinates.z);
+    }
+
+    bool Vector3::operator == (const Vector3& vector) const {
+        return this->areEquals(vector);
+    }
+
+    Vector3 Vector3::operator - () const {
+        return Vector3(-this->coordinates.x, -this->coordinates.y, -this->coordinates.z);
+    }
+
+    Vector3 Vector3::operator * (const Scalar& k) const {
+        return Vector3(this->coordinates.x * k, 
+                this->coordinates.y * k,
+                this->coordinates.z * k);
+    }
+
+    Vector3 Vector3::operator / (const Scalar& k) const {
+        return Vector3(this->coordinates.x / k, 
+                this->coordinates.y / k,
+                this->coordinates.z / k);
+    }
+
+    void Vector3::operator += (const Vector3& vector) {
+        this->coordinates.x += vector.coordinates.x;
+        this->coordinates.y += vector.coordinates.y;
+        this->coordinates.z += vector.coordinates.z;
+    }
+
+    void Vector3::operator -= (const Vector3& vector) {
+        this->coordinates.x -= vector.coordinates.x;
+        this->coordinates.y -= vector.coordinates.y;
+        this->coordinates.z -= vector.coordinates.z;
+    }
+
+    void Vector3::operator *= (const Scalar& k) {
+        this->coordinates.x *= k;
+        this->coordinates.y *= k;
+        this->coordinates.z *= k;
+    }
+
+    void Vector3::operator /= (const Scalar& k) {
+        this->coordinates.x /= k;
+        this->coordinates.y /= k;
+        this->coordinates.z /= k;
+    }
+
+    // TODO: Convert cosine from radians to degree
+    Scalar Vector3::angleBetween (const Vector3& vector) const {
+        return std::acos(this->normalized() * vector.normalized());
+    }
+
+    Vector3 Vector3::lerp(const Vector3& vector, const Scalar& t) const {
+        return *this * (1 - t) + vector * t;
+    }
+
+    bool Vector3::isZero() const {
+        return this->squareMagnitude() == 0;
+    }
+
+    bool Vector3::areEquals(const Vector3& vector) const{
+        return (*this - vector).isZero();
+    }
+
+    void Vector3::print() const {
+        std::cout << "( " << this->coordinates.x << ", " << this->coordinates.y << ", " << this->coordinates.z << " )" << std::endl;
+    }
+}
