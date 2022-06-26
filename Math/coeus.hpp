ce79cb978040597ffa2fee31986a7e928c898d51
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-06-26 17:00:21 +0200

    Implementation of simple header includer (to change later)

---
+++ Math/coeus.hpp
@@ -0,0 +1,16 @@
+#ifndef COEUS_HPP
+#define COEUS_HPP
+
+#include <Vector2.hpp>
+#include <Vector3.hpp>
+#include <Vector4.hpp>
+
+#include <Matrix2.hpp>
+#include <Matrix3.hpp>
+#include <Matrix4.hpp>
+
+#include <Math.hpp>
+#include <Scalar.hpp>
+#include <Quaternion.hpp>
+
+#endif
