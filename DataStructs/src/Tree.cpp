d40b5b73e8d097b112683152e72bc1182675bdf3
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-06-27 14:22:20 +0200

    Fixed circular dependency inside Tree

--- DataStructs/src/Tree.cpp
+++ DataStructs/src/Tree.cpp
@@ -2,22 +2,32 @@
 
 namespace Zeus {
     Tree::Tree() {
-        root.father = nullptr;
-        root.transform = nullptr;
+        root = new Node;
+
+        root->father = nullptr;
+        root->transform = nullptr;
+    }
+
+    Tree::Tree(Tree* t) {
+        this->root = new Node;
+        
+        this->root = t->root;
     }
 
     Tree::Tree(Odysseus::Transform* rootTransform) {
-        root.father = nullptr;
-        root.transform = rootTransform;
+        root = new Node;
+        
+        root->father = nullptr;
+        root->transform = rootTransform;
     }
 
-    void Tree::addChild(Node& node) {
-        root.children.push_back(node);
+    void Tree::addChild(Node* node) {
+        root->children.push_back(node);
     }
 
-    void Tree::addChildTree(Tree* tree) {
-        tree->root.father = &root;
-        root.children.push_back(tree->root);
+    void Tree::addChildTree(Tree& t) {
+        t.root->father = this->root;
+        this->root->children.push_back(t.root);
     }
 
     void Tree::deleteChild(const int& i) {
@@ -39,24 +49,30 @@ namespace Zeus {
         //     throw std::invalid_argument("EMPTY_TREE::In Tree getChild(name) the tree is currently empty, please fill the Tree before asking for children.");
     }
 
-    Node& Tree::getChild(const int& i) {
-        if (root.children.size() == 0)
-            throw std::invalid_argument("EMPTY_TREE::In Tree getChild(name) the tree is currently empty, please fill the Tree before asking for children.");
+    // Node& Tree::getChild(const int& i) {
+    //     if (root.children.size() == 0)
+    //         throw std::invalid_argument("EMPTY_TREE::In Tree getChild(name) the tree is currently empty, please fill the Tree before asking for children.");
         
-        if (i < 0 || i > root.children.size())
-            std::invalid_argument("INDEX_OUT_OF_RANGE::in Tree the index is out of range");
+    //     if (i < 0 || i > root.children.size())
+    //         std::invalid_argument("INDEX_OUT_OF_RANGE::in Tree the index is out of range");
 
-        return root.children[i];
+    //     return root.children[i];
+    // }
+
+    // Node& Tree::getChild(const std::string& name) {
+    //     if (root.children.size() == 0)
+    //         throw std::invalid_argument("EMPTY_TREE::In Tree getChild(name) the tree is currently empty, please fill the Tree before asking for children.");
+
+    //     // for (int i = 0; i < root.children.size(); i++)
+    //     //     if (root.children[i].transform->name == name)
+    //     //         return root.children[i];
+        
+    //     throw std::invalid_argument("INDEX_OUT_OF_RANGE::In Tree getChild(name) could not find any child that has that specified name");
+    // }
+
+    Tree::~Tree()
+    {
+        delete this->root;
     }
 
-    Node& Tree::getChild(const std::string& name) {
-        if (root.children.size() == 0)
-            throw std::invalid_argument("EMPTY_TREE::In Tree getChild(name) the tree is currently empty, please fill the Tree before asking for children.");
-
-        // for (int i = 0; i < root.children.size(); i++)
-        //     if (root.children[i].transform->name == name)
-        //         return root.children[i];
-        
-        throw std::invalid_argument("INDEX_OUT_OF_RANGE::In Tree getChild(name) could not find any child that has that specified name");
-    }
 }
