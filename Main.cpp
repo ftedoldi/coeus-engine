5964343a08f877be696eb89f7e7dd346c01a8f2a
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-06-28 19:48:06 +0200

    Fixed main after wrong merge

--- Main.cpp
+++ Main.cpp
@@ -13,9 +13,6 @@
 
 #include <iostream>
 
-#include <Transform.hpp>
-#include <Tree.hpp>
-
 //#include <Test.cuh>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -41,11 +38,6 @@ float lastFrame = 0.0f;
 
 int main()
 {
-    Odysseus::Transform* transform = new Odysseus::Transform();
-	transform->position.print();
-	Zeus::Tree* t = new Zeus::Tree(transform);
-	t->root->transform->position.print();
-
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -177,50 +169,6 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
     float xpos = static_cast<float>(xposIn);
     float ypos = static_cast<float>(yposIn);
-
-    glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-	// render loop
-	// -----------
-	while (!glfwWindowShouldClose(window))
-	{
-		// input
-		// -----
-		processInput(window);
-
-		// render
-		// ------
-		// glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// glfw: terminate, clearing all previously allocated GLFWresources.
-	//---------------------------------------------------------------
-	glfwTerminate();
-	return 0;
 
     if (firstMouse)
     {
