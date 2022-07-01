fb836fe8021a10ac11d1d897e4b7236548047490
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-07-01 14:19:33 +0200

    Moved get add remove components methods in SceneObject

--- Rendering/Scene/Container.hpp
+++ Rendering/Scene/Container.hpp
@@ -14,30 +14,15 @@ namespace Odysseus {
     class Transform;
 
     class Container {
+        friend class SceneObject;
+        
         private:
             std::vector<Component*> _components;
-            SceneObject& _owner;
-            Transform& _ownerTransform;
 
         public:
             const std::vector<Component*>& components;
 
             Container(SceneObject& owner, Transform& ownerTransform);
-
-            // template<class T> void addComponent() {
-            //     components.push_back(new T());
-            // }
-
-            // template<class T> T* getComponent() {
-            //     for (int i = 0; i < components.size(); i++) {
-            //         if (components[i]->getUniqueID() == T.getUniqueID())
-            //             return (T*)components[i];
-            //     }
-            // }
-
-            void addComponent(Component& component);
-            Component* getComponent(Component& component);
-            void removeComponent(Component& component);
 
             bool operator == (Container& b);
 
