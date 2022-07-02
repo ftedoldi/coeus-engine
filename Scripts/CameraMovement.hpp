3cc698d0f7e571f65daed1a0a6dcfcbb398fe33c
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-07-02 23:44:23 +0200

    Added folder with custom behaviors for our components

---
+++ Scripts/CameraMovement.hpp
@@ -0,0 +1,10 @@
+#ifndef __CAMERAMOVEMENT_H__
+#define __CAMERAMOVEMENT_H__
+
+#include <Component.hpp>
+
+class CameraMovement /*: public Odysseus::Component*/ { 
+
+};
+
+#endif // __CAMERAMOVEMENT_H__
