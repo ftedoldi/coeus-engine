f9e3035f342b02af5f1e751593269f583d236004
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-07-05 18:46:37 +0200

    Added behaviour class for all the user custom components

--- System/Behaviour.hpp
+++ System/Behaviour.hpp
@@ -1,9 +1,20 @@
 #ifndef __BEHAVIOUR_H__
 #define __BEHAVIOUR_H__
 
+#include <Component.hpp>
+
 namespace System
 {
-    
+    class Behaviour : public Component {
+        virtual void start() = 0;
+        virtual void update() = 0;
+
+        virtual void setOrderOfExecution(const short& newOrderOfExecution) = 0;
+
+        virtual short getUniqueID() = 0;
+
+        virtual std::string toString() = 0;
+    };
 }
 
 
