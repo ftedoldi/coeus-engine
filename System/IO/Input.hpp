7f928fdcac7de35c9cd6e57a02b090fc4f683761
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-07-05 18:42:08 +0200

    Added IO util class for mouse and keyboard

--- System/IO/Input.hpp
+++ System/IO/Input.hpp
@@ -1,10 +1,25 @@
 #ifndef __INPUT_H__
 #define __INPUT_H__
 
+#include <Scalar.hpp>
+
 namespace System
 {
-    class Input {
+    struct Mouse
+    {
+        bool isFirstMovement;
 
+        Athena::Scalar xPosition;
+        Athena::Scalar yPosition;
+
+        Athena::Scalar xOffsetFromLastPosition;
+        Athena::Scalar yOffsetFromLastPosition;
+    };
+    
+    // TODO: Implement method in order to get a key pressed event
+    class Input {
+        public:
+            static Mouse mouse;
     };
 }
 
---
+++ System/IO/src/Input.cpp
@@ -0,0 +1,5 @@
+#include <IO/Input.hpp>
+
+namespace System {
+    Mouse Input::mouse;
+}
