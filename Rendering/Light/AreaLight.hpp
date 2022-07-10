064165622adcb73bb98fa556675194f0b080ea1d
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-07-10 20:11:56 +0200

    Implemented AreaLight as an array of PointLights

--- Rendering/Light/AreaLight.hpp
+++ Rendering/Light/AreaLight.hpp
@@ -1,11 +1,28 @@
 #ifndef AREALIGHT_HPP
 #define AREALIGHT_HPP
 
+#include <PointLight.hpp>
+
 namespace Odysseus
 {
-    class AreaLight
+    class AreaLight : public Light
     {
+        private:
+        std::vector<PointLight> pointLights;
 
+        public:
+
+        AreaLight();
+        virtual void start();
+        virtual void update();
+
+        virtual void setOrderOfExecution(const short& newOrderOfExecution);
+
+        virtual short getUniqueID();
+
+        virtual std::string toString();
+        void addLights(PointLight& pt);
+        void setLightShader(Odysseus::Shader* shader) const;
     };
 }
 
