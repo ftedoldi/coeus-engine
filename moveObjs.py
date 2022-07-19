33a822dad9ee30748e362a85d39c4e32d8367feb
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-07-19 12:21:15 +0200

    Fixed moveObjs script in order to be able to compile a class from scratch

--- moveObjs.py
+++ moveObjs.py
@@ -7,9 +7,13 @@ objs += [each for each in os.listdir(".") if each.endswith('.obj')]
 pdb = []
 pdb += [each for each in os.listdir(".") if each.endswith('.pdb')]
 
-for val in pdb:
-    os.remove("./" + str(val))
+try:
+    for val in pdb:
+        os.remove("./" + str(val))
 
-for element in objs:
-    os.remove("./.objs/" + str(element))
-    shutil.move("./" + str(element), "./.objs/" + str(element))
+finally:
+    for element in objs:
+        try:
+            os.remove("./.objs/" + str(element))
+        finally:
+            shutil.move("./" + str(element), "./.objs/" + str(element))
