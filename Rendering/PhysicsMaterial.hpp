0b5fbb225a6358aaca239dfff01b7b8cd45ff11b
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-07-24 18:31:09 +0200

    First implementation of PhysicsMaterial

--- Rendering/PhysicsMaterial.hpp
+++ Rendering/PhysicsMaterial.hpp
@@ -1,10 +1,19 @@
 #ifndef __PHYSICSMATERIAL_H__
 #define __PHYSICSMATERIAL_H__
+#include <Vector3.hpp>
+#include <vector>
+#include <Texture2D.hpp>
 
 namespace Odysseus
 {
     class PhysicsMaterial {
 
+        public:
+            Athena::Vector3 color;
+            float metallic;
+            float roughness;
+            float AO;
+            std::vector<Texture2D> PBR_textures;
     };
 } // namespace Odysseus
 
