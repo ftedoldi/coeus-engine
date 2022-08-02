a20d6dcf6fe473b3e43d36ef41317aa4ab076bb4
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-08-02 19:06:02 +0200

    Dockspace icons are not getting gamma corrected

--- System/src/Dockspace.cpp
+++ System/src/Dockspace.cpp
@@ -375,7 +375,7 @@ namespace System {
                 ImGui::ImageButtonEx(
                                             100,
                                             (ImTextureID)Odysseus::Texture2D::loadTextureFromFile(
-                                                (Folder::getFolderPath("Icons").string() + "/leftArrow.png").c_str()
+                                                (Folder::getFolderPath("Icons").string() + "/leftArrow.png").c_str(), false
                                             ).ID, 
                                             { iconScale, iconScale },
                                             { 0, 0 },
@@ -397,7 +397,7 @@ namespace System {
                 ImGui::ImageButtonEx(
                                             100,
                                             (ImTextureID)Odysseus::Texture2D::loadTextureFromFile(
-                                                (Folder::getFolderPath("Icons").string() + "/leftArrow.png").c_str()
+                                                (Folder::getFolderPath("Icons").string() + "/leftArrow.png").c_str(), false
                                             ).ID, 
                                             { iconScale, iconScale },
                                             { 1, 1 },
@@ -416,7 +416,7 @@ namespace System {
                 ImGui::ImageButtonEx(
                                             100,
                                             (ImTextureID)Odysseus::Texture2D::loadTextureFromFile(
-                                                (Folder::getFolderPath("Icons").string() + "/rotate.png").c_str()
+                                                (Folder::getFolderPath("Icons").string() + "/rotate.png").c_str(), false
                                             ).ID, 
                                             { iconScale, iconScale },
                                             { 1, 1 },
@@ -477,7 +477,7 @@ namespace System {
                         ImGui::ImageButtonEx(
                                                 ++index,
                                                 (ImTextureID)Odysseus::Texture2D::loadTextureFromFile(
-                                                    (Folder::getFolderPath("Icons").string() + "/folder.png").c_str()
+                                                    (Folder::getFolderPath("Icons").string() + "/folder.png").c_str(), false
                                                 ).ID, 
                                                 { thumbnailSize, thumbnailSize },
                                                 { 1, 1 },
@@ -503,7 +503,7 @@ namespace System {
                         ImGui::ImageButtonEx(
                                                     ++index,
                                                     (ImTextureID)Odysseus::Texture2D::loadTextureFromFile(
-                                                        (Folder::getFolderPath("Icons").string() + "/document.png").c_str()
+                                                        (Folder::getFolderPath("Icons").string() + "/document.png").c_str(), false
                                                     ).ID, 
                                                     { thumbnailSize, thumbnailSize },
                                                     { 1, 1 },
