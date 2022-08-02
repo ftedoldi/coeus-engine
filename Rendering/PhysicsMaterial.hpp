9299ad5d809f35653df2f0a86bdd810d7556d9d6
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-08-02 19:01:59 +0200

    Implemented PBR material manager

--- Rendering/PhysicsMaterial.hpp
+++ Rendering/PhysicsMaterial.hpp
@@ -3,17 +3,25 @@
 #include <Vector3.hpp>
 #include <vector>
 #include <Texture2D.hpp>
+#include <Shader.hpp>
 
 namespace Odysseus
 {
     class PhysicsMaterial {
 
         public:
-            Athena::Vector3 color;
+            Athena::Vector3 albedo;
             float metallic;
             float roughness;
             float AO;
             std::vector<Texture2D> PBR_textures;
+
+        PhysicsMaterial();
+        PhysicsMaterial(Athena::Vector3& color, float metallic, float roughness);
+        PhysicsMaterial(std::vector<Texture2D>& PBR_textures);
+
+        void loadShaderMaterial(Odysseus::Shader* shader);
+        void loadShaderTexture(Odysseus::Shader* shader);
     };
 } // namespace Odysseus
 
