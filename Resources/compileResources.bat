ac22107788617c946a4a132924d09e4e5f643f25
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-08-02 11:36:35 +0200

    Added Resource folder with icons and a script that compiles our resolution (.rc) file into a .res file in order to set the default .exe icon

---
+++ Resources/compileResources.bat
@@ -0,0 +1,4 @@
+cd Resources
+
+@REM in order to compile you need to adjust this absoulute path, unluckily there is no way to set it up for everyone
+call "C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86\rc.exe" resource.rc
