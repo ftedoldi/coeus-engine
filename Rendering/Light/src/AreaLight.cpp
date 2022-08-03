361a0c553f8272687b272a204ad34f36b8e4fdad
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-08-03 13:01:03 +0200

    Modified part of AreaLight class (still has some problems)

--- Rendering/Light/src/AreaLight.cpp
+++ Rendering/Light/src/AreaLight.cpp
@@ -2,7 +2,6 @@
 
 namespace Odysseus
 {
-
     AreaLight::AreaLight()
     {
 
@@ -32,24 +31,27 @@ namespace Odysseus
         return "AreaLight";
     }
 
-    void AreaLight::addLights(PointLight& pt)
+    void AreaLight::addLight(PointLight* pt)
     {
         this->pointLights.push_back(pt);
     }
 
     void AreaLight::setLightShader(Odysseus::Shader* shader) const
     {
-        shader->use();
+        this->shader->use();
         
         for(unsigned int i = 0; i < this->pointLights.size(); ++i)
         {
-            shader->setVec3("pointLights[i].position", pointLights[i].getPosition());
-            shader->setVec3("pointLights[i].ambient", pointLights[i].getAmbient());
-            shader->setVec3("pointLights[i].diffuse", pointLights[i].getDiffuse());
-            shader->setVec3("pointLights[i].specular", pointLights[i].getSpecular());
-            shader->setFloat("pointLights[i].constant", pointLights[i].getConstant());
-            shader->setFloat("pointLights[i].linear", pointLights[i].getLinear());
-            shader->setFloat("pointLights[i].quadratic", pointLights[i].getQuadratic());
+            std::cout << "pointlight" << i << " position: ";
+            pointLights[i]->getPosition().print();
+            std::cout << std::endl;
+            shader->setVec3("pointLights[i].position", pointLights[i]->getPosition());
+            shader->setVec3("pointLights[i].ambient", pointLights[i]->getAmbient());
+            shader->setVec3("pointLights[i].diffuse", pointLights[i]->getDiffuse());
+            shader->setVec3("pointLights[i].specular", pointLights[i]->getSpecular());
+            shader->setFloat("pointLights[i].constant", pointLights[i]->getConstant());
+            shader->setFloat("pointLights[i].linear", pointLights[i]->getLinear());
+            shader->setFloat("pointLights[i].quadratic", pointLights[i]->getQuadratic());
         }
     }
 
