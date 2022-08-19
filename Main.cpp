3cec43453e16a3a85c101abd72cfc7116bfcc297
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-08-19 23:58:37 +0200

    Updated main in order to deserialize a scene instead of statically creating one

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,10 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
+#include <Component.hpp>
 #include <coeus.hpp>
 #include <Shader.hpp>
-#include <camera.hpp>
+#include <EditorCamera.hpp>
 #include <Model.hpp>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
@@ -12,8 +13,7 @@
 
 #include <Texture2D.hpp>
 #include <SceneObject.hpp>
-#include <SceneGraph.hpp>
-#include <Component.hpp>
+#include <SceneManager.hpp>
 #include <Time.hpp>
 #include "Test/DummyComponent.hpp"
 #include <PointLight.hpp>
@@ -23,13 +23,35 @@
 #include <AreaLight.hpp>
 
 #include <Serializer/Serializer.hpp>
+#include <SerializableClass.hpp>
+
+#include <rttr/registration>
 
 #include <iostream>
 #include <vector>
+#include <thread>
 
 #include "Scripts/CameraMovement.hpp"
 
-//#include <Test.cuh>
+
+// #include <Python.h>
+
+// #include <Test.cuh>
+
+// void runPythonScript()
+// {
+//     char filename[] = "test.py";
+//     FILE* fp;
+
+//     Py_Initialize();
+//     fp = _Py_fopen(filename, "r");
+//     PyRun_SimpleFile(fp, filename);
+//     Py_Finalize();
+// }
+
+// TODO: Implement Editor Camera Movement
+// TODO: Avoid Showing editor camera in hierarchy
+// TODO: Store (or better) serialize Light components
 
 int main()
 {
@@ -38,92 +60,100 @@ int main()
     // Where all the starts are runned
     System::Window* window = new System::Window("myWindow");
 
-    //Dummy component test
-    System::Component *c = new DummyComponent();
-    Odysseus::SceneObject *obj = new Odysseus::SceneObject();
-    obj->transform->name = "Object";
-    obj->addComponent<DummyComponent>();
+    serializer.deserialize("./Assets/Scenes/Test.coeus");
+
+    // Odysseus::Scene* startScene = new Odysseus::Scene(std::string("Start Scene"));
+    // Odysseus::SceneManager::addScene(startScene);
+    // // TODO: Check if there is a default scene loaded
+    // // TODO: Create a default configuration file in order to set the default scene to load
+    // Odysseus::SceneManager::setActiveScene(0);
+
+
+    // //Dummy component test
+    // System::Component *c = new DummyComponent();
+    // Odysseus::SceneObject *obj = new Odysseus::SceneObject();
+    // obj->transform->name = "Object";
+    // auto dummy = obj->addComponent<DummyComponent>();
 
     //Camera setup
-    Odysseus::SceneObject *cam = new Odysseus::SceneObject();
-    cam->transform->position = Athena::Vector3(0, 0, 20);
-    cam->transform->name = "Camera";
-    auto mainCamera = cam->addComponent<Odysseus::Camera>();
-    Odysseus::Camera::main = mainCamera;
+    // Odysseus::SceneObject *cam = new Odysseus::SceneObject();
+    // cam->transform->position = Athena::Vector3(0, 0, 20);
+    // cam->transform->name = "EditorCamera";
+    // auto mainEditorCamera = cam->addComponent<Odysseus::EditorCamera>();
+    // Odysseus::EditorCamera::main = mainEditorCamera;
 
-    //Camera movement setup
-    auto movement = cam->addComponent<CameraMovement>();
-    movement->camera = mainCamera;
-    cam->getComponent<CameraMovement>()->camera = cam->getComponent<Odysseus::Camera>();
+    // //Camera movement setup
+    // auto movement = cam->addComponent<CameraMovement>();
+    // movement->camera = mainCamera;
+    // cam->getComponent<CameraMovement>()->camera = cam->getComponent<Odysseus::EditorCamera>();
 
-    //-------------------------------------------------------
-    //Shaders setup
+    // //-------------------------------------------------------
+    // //Shaders setup
 
     Odysseus::Shader* modelShader = new Odysseus::Shader(".\\Shader\\phongShader.vert", ".\\Shader\\phongShader.frag");
     Odysseus::Shader* PBRshader = new Odysseus::Shader(".\\Shader\\PBRshader.vert", ".\\Shader\\PBRshader.frag");
 
-    //-------------------------------------------------------
-    //Models setup
+    // //-------------------------------------------------------
+    // //Models setup
 
-    Odysseus::Model myModel("Assets/Models/PBRsphere/PBRsphere.obj", modelShader, false);
-    Odysseus::Model myModel1("Assets/Models/matAndTex/matAndTex.obj", modelShader, false);
+    // Odysseus::Model myModel("Assets/Models/PBRsphere/PBRsphere.obj", modelShader, false);
+    // Odysseus::Model myModel1("Assets/Models/matAndTex/matAndTex.obj", modelShader, false);
     // Odysseus::Model myModel1("Assets/Models/testFiles/posCubesColored.gltf", modelShader, false);
-    // myModel.setIfPBR(true);
-    // myModel1.setIfPBR(true);
+    // // myModel.setIfPBR(true);
+    // // myModel1.setIfPBR(true);
 
-    //-------------------------------------------------------
-    //Light setup
+    // //-------------------------------------------------------
+    // //Light setup
 
-    //Point light
-    Odysseus::SceneObject *light = new Odysseus::SceneObject();
-    light->transform->name = "PointLight";
-    auto pLight = light->addComponent<Odysseus::PointLight>();
+    // //Point light
+
+    // Odysseus::SceneObject *light = new Odysseus::SceneObject();
+    // light->transform->name = "PointLight";
+    // auto pLight = light->addComponent<Odysseus::PointLight>();
     
-    pLight->setPosition(Athena::Vector3(0.0f, 2.0f, 0.0f));
-    pLight->setShader(modelShader);
-    pLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
-    pLight->setDiffuse(Athena::Vector3(0.8f, 0.8f, 0.8f));
-    pLight->setSpecular(Athena::Vector3(0.3f, 0.3f, 0.3f));
-    pLight->setConstant(1.0f);
-    pLight->setLinear(0.09f);
-    pLight->setQuadratic(0.032f);
+    // pLight->setPosition(Athena::Vector3(0.0f, 2.0f, 0.0f));
+    // pLight->setShader(modelShader);
+    // pLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
+    // pLight->setDiffuse(Athena::Vector3(0.8f, 0.8f, 0.8f));
+    // pLight->setSpecular(Athena::Vector3(0.3f, 0.3f, 0.3f));
+    // pLight->setConstant(1.0f);
+    // pLight->setLinear(0.09f);
+    // pLight->setQuadratic(0.032f);
 
-    //Directional light
-    Odysseus::SceneObject *dirLight = new Odysseus::SceneObject();
-    dirLight->transform->name = "DirectionaLight";
-    auto dLight = dirLight->addComponent<Odysseus::DirectionalLight>();
 
-    dLight->setShader(modelShader);
-    dLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
-    dLight->setDiffuse(Athena::Vector3(0.5f, 0.5f, 0.5f));
-    dLight->setSpecular(Athena::Vector3(0.5f, 0.5f, 0.5f));
-    dLight->setDirection(Athena::Vector3(0.0f, -1.0f, 0.0f));
+    // //Directional light
+    // Odysseus::SceneObject *dirLight = new Odysseus::SceneObject();
+    // dirLight->transform->name = "DirectionaLight";
+    // auto dLight = dirLight->addComponent<Odysseus::DirectionalLight>();
+
+    // dLight->setShader(modelShader);
+    // dLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
+    // dLight->setDiffuse(Athena::Vector3(0.5f, 0.5f, 0.5f));
+    // dLight->setSpecular(Athena::Vector3(0.5f, 0.5f, 0.5f));
+    // dLight->setDirection(Athena::Vector3(0.0f, -1.0f, 0.0f));
     
-    //Spot light
-    Odysseus::SceneObject *spotLight = new Odysseus::SceneObject();
-    spotLight->transform->name = "spotLight";
-    auto sLight = spotLight->addComponent<Odysseus::SpotLight>();
+    // //Spot light
+    // Odysseus::SceneObject *spotLight = new Odysseus::SceneObject();
+    // spotLight->transform->name = "spotLight";
+    // auto sLight = spotLight->addComponent<Odysseus::SpotLight>();
     
-    sLight->setPosition(Athena::Vector3(0.0f, 2.0f, 0.0f));
-    sLight->setDirection(Athena::Vector3(0.0f, -1.0f, 0.0f));
-    sLight->setShader(modelShader);
-    sLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
-    sLight->setDiffuse(Athena::Vector3(0.8f, 0.8f, 0.8f));
-    sLight->setSpecular(Athena::Vector3(1.0f, 1.0f, 1.0f));
-    sLight->setCutOff(41.0f);
-    sLight->setSpotExponent(19.0f);
+    // sLight->setPosition(Athena::Vector3(0.0f, 2.0f, 0.0f));
+    // sLight->setDirection(Athena::Vector3(0.0f, -1.0f, 0.0f));
+    // sLight->setShader(modelShader);
+    // sLight->setAmbient(Athena::Vector3(0.2f, 0.2f, 0.2f));
+    // sLight->setDiffuse(Athena::Vector3(0.8f, 0.8f, 0.8f));
+    // sLight->setSpecular(Athena::Vector3(1.0f, 1.0f, 1.0f));
+    // sLight->setCutOff(41.0f);
+    // sLight->setSpotExponent(19.0f);
 
     stbi_set_flip_vertically_on_load(true);
     
-    //HDR map setup
+    // HDR map setup
     Odysseus::Cubemap* HDRImap = new Odysseus::Cubemap();
     HDRImap->setPBRshader(PBRshader);
 
-    //Setup everything before initializeScene call
-    Odysseus::SceneGraph::initializeScene();
-    Odysseus::SceneGraph::name = "Base Scene";
-
-    serializer.serialize("Assets/Scenes/Test.coeus");
+    // //Setup everything before initializeScene call
+    Odysseus::SceneManager::initializeActiveScene();
 
     // render loop
     // -----------
@@ -133,12 +163,14 @@ int main()
         // ------
         window->clear();
         
-        Odysseus::SceneGraph::drawScene();
+        Odysseus::SceneManager::drawActiveScene();
 
         HDRImap->update();
         
         window->update();
     }
+
+    serializer.serialize("Assets/Scenes/Test.coeus");
 
     delete window;
     return 0;
