1573f1301559e19b34d7f9bbd0779d4213d5bbd5
Davide Paolillo <davide.paolillo@studio.unibo.it>
2022-08-26 18:29:38 +0200

    Util class used in order to ease the process of serializing a class

--- System/SerializableClass.hpp
+++ System/SerializableClass.hpp
@@ -3,16 +3,31 @@
 
 #include <Component.hpp>
 
+#include <rttr/registration>
+
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <vector>
+
+// Specify the parent class/classes here
+#define SERIALIZABLE_CLASS(...) RTTR_ENABLE(__VA_ARGS__)
+
+// Use this macro to specify wich class you wish to register
+#define SERIALIZE_CLASS RTTR_REGISTRATION
+
+// Use this macro in order to get a variable name
+#define NAMEOF(variable) ((decltype(&variable))nullptr, #variable)
 
 namespace System
 {
     class SerializableClass
     {
         public:
-            static std::unordered_map<std::string, size_t> m;
+            template<class T>
+            static rttr::registration::class_<T> registerClass(std::string name)
+            {
+                return rttr::registration::class_<T>(name).constructor<>()(rttr::policy::ctor::as_raw_ptr);
+            }
     };
 } // namespace System
 
