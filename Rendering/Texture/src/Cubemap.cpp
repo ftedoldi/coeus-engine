a0a8a0bcce0ebc219b4acba76d6c6f06069723f4
Francesco <francesco.tedoldi.unimi@gmail.com>
2022-11-03 11:07:26 +0100

    Incremented reflection resolution

--- Rendering/Texture/src/Cubemap.cpp
+++ Rendering/Texture/src/Cubemap.cpp
@@ -168,7 +168,7 @@ namespace Odysseus
         glBindTexture(GL_TEXTURE_CUBE_MAP, this->prefilterMap);
         for (unsigned int i = 0; i < 6; ++i)
         {
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 128, 128, 0, GL_RGB, GL_FLOAT, nullptr);
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 512, 512, 0, GL_RGB, GL_FLOAT, nullptr);
         }
         glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -190,8 +190,8 @@ namespace Odysseus
         for (unsigned int mip = 0; mip < maxMipLevels; ++mip)
         {
             // resize framebuffer according to mip-level size.
-            unsigned int mipWidth = static_cast<unsigned int>(128 * std::pow(0.5, mip));
-            unsigned int mipHeight = static_cast<unsigned int>(128 * std::pow(0.5, mip));
+            unsigned int mipWidth = static_cast<unsigned int>(512 * std::pow(0.5, mip));
+            unsigned int mipHeight = static_cast<unsigned int>(512 * std::pow(0.5, mip));
             glBindRenderbuffer(GL_RENDERBUFFER, this->captureRBO);
             glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, mipWidth, mipHeight);
             glViewport(0, 0, mipWidth, mipHeight);
